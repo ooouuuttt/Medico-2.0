4b3ff51a969838de93f65b94fb798f1541d4071c
Mohammad Murtaza Tamboli <murtazawaheedtamboli@gmail.com>
2025-09-18 06:26:31 +0000

    this error is occuring

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  }
 };
 
 export default nextConfig;
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   projectId: 'studio-9797456116-e1010',
   appId: '1:126430681217:web:6161791df6da3aee971580',
   storageBucket: 'studio-9797456116-e1010.firebasestorage.app',
-  apiKey: 'AIzaSyArNCekk9yCL4gihfsVFbJQP781-swBrWM',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: 'studio-9797456116-e1010.firebaseapp.com',
   measurementId: '',
   messagingSenderId: '126430681217',
